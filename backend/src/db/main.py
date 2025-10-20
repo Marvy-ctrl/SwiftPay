05daf8f0c1a8d41c57582c0a4ca2c44a107bee48
Marvel <popoolamarvellous51@gmail.com>
2025-10-20 14:01:56 +0100

    fix async db session error

--- backend/src/db/main.py
+++ backend/src/db/main.py
@@ -1,23 +1,30 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel, create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
-
-# from src.db.models import User, Account, Transaction, LuckyDraw
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from src.config import Config
 
-async_engine = create_async_engine(Config.DATABASE_URL, connect_args={"ssl": True})
+# Create engine
+async_engine: AsyncEngine = create_async_engine(
+    Config.DATABASE_URL,
+    connect_args={"ssl": True},
+    echo=True,
+)
+
+# Create sessionmaker globally
+async_session_maker = async_sessionmaker(
+    bind=async_engine, class_=AsyncSession, expire_on_commit=False
+)
 
 
+# Dependency for FastAPI routes
+async def get_session() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
+
+
+# DB initialization
 async def init_db() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
-
-
-async def get_session() -> AsyncSession:
-    Session = sessionmaker(
-        bind=async_engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with Session() as session:
-        yield session
