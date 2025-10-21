705dd55c4d01dc20970a5d4abed3d5df582f28ff
Marvel <popoolamarvellous51@gmail.com>
2025-10-21 10:26:52 +0100

    edited session

--- backend/src/db/main.py
+++ backend/src/db/main.py
@@ -5,25 +5,20 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from src.config import Config
 
-# Create engine
 async_engine: AsyncEngine = create_async_engine(
-    Config.DATABASE_URL,
-    connect_args={"ssl": True},
+    Config.DATABASE_URL, connect_args={"ssl": True}
 )
 
-# Create sessionmaker globally
 async_session_maker = async_sessionmaker(
     bind=async_engine, class_=AsyncSession, expire_on_commit=False
 )
 
 
-# Dependency for FastAPI routes
 async def get_session() -> AsyncSession:
     async with async_session_maker() as session:
         yield session
 
 
-# DB initialization
 async def init_db() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
